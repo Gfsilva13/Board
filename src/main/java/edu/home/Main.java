2d990490fefec2615fb52cee2633c910ca27f62b
GFsilva13 <genivaldo13@gmail.com>
2025-05-13 00:59:59 -0300

    Comitt inicial incluindo migrations

---
+++ src/main/java/edu/home/Main.java
@@ -0,0 +1,18 @@
+package edu.home;
+
+import edu.home.persistence.migration.MigrationStrategy;
+//import edu.home.ui.MainMenu;
+
+import java.sql.SQLException;
+
+import static edu.home.persistence.config.ConnectionConfig.getConnection;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        try(var connection = getConnection()){
+            new MigrationStrategy(connection).executeMigration();
+        }
+        //new MainMenu().execute();
+
+    }
+}
